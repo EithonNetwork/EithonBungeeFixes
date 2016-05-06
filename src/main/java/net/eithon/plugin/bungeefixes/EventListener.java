26475d07eca0025600984e4aac9a9c6ddfc10db2
vanbrayne <lars.lindberg@xlent.se>
2016-05-06 09:42:57 +0200

    PlayerDisconnectedEvent

--- src/main/java/net/eithon/plugin/bungeefixes/EventListener.java
+++ src/main/java/net/eithon/plugin/bungeefixes/EventListener.java
@@ -1,18 +1,23 @@
 package net.eithon.plugin.bungeefixes;
 
+import net.eithon.plugin.bungeefixes.logic.Controller;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class EventListener implements Listener {
+	
+	private Controller _controller;
 
-    public EventListener(ProxyServer proxy) {
+    public EventListener(ProxyServer proxy, Controller controller) {
 		super();
+		this._controller = controller;
 	}
 
 	@EventHandler
-    public void onPostLogin(PostLoginEvent event) {
+    public void onPlayerDisconnectEvent(PlayerDisconnectEvent event) {
+		this._controller.playerDisconnnected(event.getPlayer());
     }
 
 }
